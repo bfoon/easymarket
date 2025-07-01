5ab92723b01b8f6174f5b60a07df344c51edd05e
babou <baboucarrfoon@gmail.com>
2025-07-01 11:02:12 +0000

    push8

--- marketplace/models.py
+++ marketplace/models.py
@@ -15,6 +15,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
+    specifications = models.TextField()
     image = models.ImageField(upload_to='products/')
     is_featured = models.BooleanField(default=False)
     is_trending = models.BooleanField(default=False)
